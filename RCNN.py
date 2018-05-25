516a6b6b2dd8c0178ca9303e531bd88c2dddaf71
sangeet259 <mail2sangeetmishra@gmail.com>
2018-05-25 21:53:08 +0530

    adds imports and initial transforms on CIFAR

---
+++ RCNN.py
@@ -0,0 +1,23 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import pudb
+import torch.nn as nn
+import torch.nn.functional as F
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
